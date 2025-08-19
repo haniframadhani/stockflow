35a34766d27840bf70313b1f11a70c7c30d862fa
muhammad hanif ramadhani <mhaniframadhani985@gmail.com>
2025-08-19 20:53:21 +0700

    fix: correct import path for globals.css in AuthLayout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../globals.css";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 
 export const metadata: Metadata = {
